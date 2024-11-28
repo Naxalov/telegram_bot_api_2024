c3040aa0f6193fc4eaa508d65b943134bb40b7ab
m1 <uzbzarif@gmail.com>
2024-11-28 19:01:07 +0500

    send message

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import os
 
 TOKEN = os.getenv('TOKEN')
 base_url = f'https://api.telegram.org/bot{TOKEN}'
-r = requests.get(f'{base_url}/getUpdates')
-result = r.json()['result'][0]
-message = result['message']
-print(message['text'])
+
+chat_id = 86775091
+text = 'hello'
+
+r = requests.get(f'{base_url}/sendMessage?chat_id={chat_id}&text={text}')
+
+print(r.json())
