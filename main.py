04e44d2c76b228304a0cf6a03ea21f05474bb545
m1 <uzbzarif@gmail.com>
2024-11-28 18:58:17 +0500

    get text

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ import os
 
 TOKEN = os.getenv('TOKEN')
 base_url = f'https://api.telegram.org/bot{TOKEN}'
-r = requests.get(f'{base_url}/getMe')
-print(r.json())
+r = requests.get(f'{base_url}/getUpdates')
+result = r.json()['result'][0]
+message = result['message']
+print(message['text'])
